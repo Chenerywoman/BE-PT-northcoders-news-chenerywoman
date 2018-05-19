518e45898a1489d40999dc7e01f3a610039eb38f
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-19 15:55:18 +0100

    passing test for getAllTopics function

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,11 +1,15 @@
-// const {findAllTopics, findArticlesByTopicId} = require('../queries/topics.queries');
+const {findAllTopics, findTopicById, findArticlesByTopic} = require('../queries/topics.queries');
 
 exports.getAllTopics = (req, res, next) => {
-  return res.send('in getAllTopics');
+  return findAllTopics()
+  .then(topics => res.status(200).send({topics}))
+  .catch(err => next({status: 500, message: 'unable to find topics'}))
 };
-
+  
 exports.getArticlesByTopicId = (req, res, next) => {
-    return res.send('getArticlesByTopicId');
+    return findTopicById(req.params.topic_id)
+    .then(topic => findArticlesByTopic(topic._id))
+    .then(articles => res.status(200).send({articles}))
 };
 
 exports.postNewArticleToTopic = (req, res, next) => {
