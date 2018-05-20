80dcf6656c1e1050a09b926f1fa2b940337ce8a8
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-20 21:33:56 +0100

    removing comments used for setting up tests

--- queries/topics.queries.js
+++ queries/topics.queries.js
@@ -1,21 +1,7 @@
 const {Topic, Article} = require('../models/');
 
-
 exports.findAllTopics = () => Topic.find();
 
 exports.findTopicById = (id) => Topic.findById(id);
 
 exports.findArticlesByTopicId = (belongs_to) => Article.find({belongs_to})
-
-// article
-// {   "_id": "583412925905f02e4c8e6e00", CREATED BY mongoDB
-//     "title": "Living in the shadow of a  great man",
-//     "topic": "mitch", //  NEED TO CHANGE TO A TOPIC IDn & CHANGE NAME TO BELONGS TO
-//     "created_by": "butter_bridge", // NEED TO CHANGE TO A USER ID
-//     "body": "I find this existence challenging"
-//      "belongs_to NEED TO ADD A NEW FIELD & put topic id
-//      "__v": 0 - added by mongoosedb
-//   }
-//     "votes": 77,  DEFAULT in SCHEMA IS 0
-//     "comments": 13  ADD THIS
-//   },
