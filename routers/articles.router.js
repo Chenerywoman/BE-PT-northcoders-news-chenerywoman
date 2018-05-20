60ae4071a454bae8a07313ec968192993c756afd
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-20 22:59:15 +0100

    correcting minor linting issues

--- routers/articles.router.js
+++ routers/articles.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllArticles, getArticlesById, getCommentsforArticle, postNewCommentToArticle,changeArticleVotes} = require('../controllers/articles.controller');
+const { getAllArticles, getArticlesById, getCommentsforArticle, postNewCommentToArticle, changeArticleVotes} = require('../controllers/articles.controller');
 
 router.get('/', getAllArticles);
 
