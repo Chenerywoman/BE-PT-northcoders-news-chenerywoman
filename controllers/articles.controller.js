a9145c1a3241905aef602bacdc8319a2be15e6a6
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-20 15:41:24 +0100

    adding content to getAllArticles & getArticlesById functions & passing tests

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,11 +1,18 @@
-// const {findAllArticles, findArticlesByTopicId} = require('../queries/articles.queries');
+const {findAllArticles, findArticleById} = require('../queries/articles.queries');
 
 exports.getAllArticles = (req, res, next) => {
-    return res.send('in getAllTArticles');
+  return findAllArticles()
+  .then(articles => res.status(200).send({articles}))
+  .catch(() => next({status: 500, message: 'server error: unable to find topics'}))
   };
   
 exports.getArticlesById = (req, res, next) => {
-      return res.send('getArticlesById');
+  return findArticleById(req.params.article_id)
+  .then(article =>  res.status(200).send({article}))
+  .catch((err) => { 
+    if (err.name === 'CastError') return next({status: 400, message: 'please input a valid article id'});
+    else return next({status: 500, message: 'server error'})
+    });
   };
 
 exports.getCommentsforArticle = (req, res, next) => {
