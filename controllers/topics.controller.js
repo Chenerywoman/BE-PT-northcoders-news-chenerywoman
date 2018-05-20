1be5f27dce39c858c1a7029bd1760e6881aca8f3
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-20 13:20:32 +0100

    completing getArticleByTopicId including error handling

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,16 +1,24 @@
-const {findAllTopics, findTopicById, findArticlesByTopic} = require('../queries/topics.queries');
+const {findAllTopics, findTopicById, findArticlesByTopicId} = require('../queries/topics.queries');
 
 exports.getAllTopics = (req, res, next) => {
   return findAllTopics()
   .then(topics => res.status(200).send({topics}))
-  .catch(err => next({status: 500, message: 'unable to find topics'}))
+  .catch(() => next({status: 500, message: 'server error: unable to find topics'}))
 };
   
 exports.getArticlesByTopicId = (req, res, next) => {
     return findTopicById(req.params.topic_id)
-    .then(topic => findArticlesByTopic(topic._id))
-    .then(articles => res.status(200).send({articles}))
-};
+    .then(topic => findArticlesByTopicId(topic._id))
+    .then(articles => {
+      if (articles.length === 0) throw {status: 404, message: 'unable to find articles for this topic' };
+      else return res.status(200).send({articles});
+    })
+    .catch((err) => { 
+      if (err.name === 'CastError') return next({status: 400, message: 'please input a valid topic id'});
+      else if (err.status === 404) return next(err);
+      else return next({status: 500, message: 'server error'})
+      });
+}; 
 
 exports.postNewArticleToTopic = (req, res, next) => {
   return res.send('in postNewArticlebyTopic');
