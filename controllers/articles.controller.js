125063c9fba8058f5e5586b52b9fa47846e1d901
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-20 16:36:15 +0100

    passing tests including error handling for getCommentsForArticle

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,4 @@
-const {findAllArticles, findArticleById} = require('../queries/articles.queries');
+const {findAllArticles, findArticleById, findCommentsForArticle} = require('../queries/articles.queries');
 
 exports.getAllArticles = (req, res, next) => {
   return findAllArticles()
@@ -11,12 +11,20 @@ exports.getArticlesById = (req, res, next) => {
   .then(article =>  res.status(200).send({article}))
   .catch((err) => { 
     if (err.name === 'CastError') return next({status: 400, message: 'please input a valid article id'});
-    else return next({status: 500, message: 'server error'})
+    else return next({status: 500, message: 'server error'});
     });
   };
 
 exports.getCommentsforArticle = (req, res, next) => {
-      return res.send('getCommentsforArticle');
+      return findCommentsForArticle(req.params.article_id)
+      .then(comments => {
+        if (comments.length === 0) throw {status: 404, message: `there are no comments for the article with id ${req.params.article_id}`};
+        else res.status(200).send({comments});
+      })
+      .catch(err => {
+        if (err.status === 404) return next(err);
+        else return next({status: 500, message: 'server error'});
+      })
   };
 
   exports.postNewCommentToArticle = (req, res, next) => {
