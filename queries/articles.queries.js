bfa76e74ae6d1091e15e0e785ef9a9962df4f24e
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-21 00:24:43 +0100

    updateArticleVote function passing tests to increase & decrease votes

--- queries/articles.queries.js
+++ queries/articles.queries.js
@@ -5,3 +5,5 @@ exports.findAllArticles = () => Article.find();
 exports.findArticleById = (id) => Article.findById(id);
 
 exports.createArticle = (title, body, belongs_to, created_by) => Article.create({title, body, belongs_to, created_by});
+
+exports.updateArticleVote  = (_id, votes) => Article.findByIdAndUpdate(_id, {votes});
