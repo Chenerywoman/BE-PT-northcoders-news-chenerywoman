07d42f6d43bdaa7022385e9528096a8ff9f20f43
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-21 00:26:26 +0100

    changeArticleVotes function passing tests to increase & decrease votes

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,4 @@
-const {findAllArticles, findArticleById} = require('../queries/articles.queries');
+const {findAllArticles, findArticleById, updateArticleVote} = require('../queries/articles.queries');
 const {findCommentsForArticle} = require('../queries/comments.queries');
 const {findUserById} = require('../queries/users.queries');
 const {createComment} = require('../queries/comments.queries')
@@ -48,5 +48,17 @@ exports.getCommentsforArticle = (req, res, next) => {
 };
 
   exports.changeArticleVotes = (req, res, next) => {
-    return res.send('articleVotes');
+    return findArticleById(req.params.article_id)
+    .then(article => {
+      const {vote} = req.query;
+      if (vote === 'up') article.votes = article.votes + 1;
+      else if (vote === 'down') article.votes = article.votes - 1;
+      return updateArticleVote(article._id, article.votes);
+      })
+    .then(article => findArticleById(article._id))
+    .then(article => res.status(200).send({updated_article: article}))
+    .catch((err) => { 
+      if (err.name === 'CastError' && err.model.modelName === 'articles') return next({ status: 400,  message: `${req.params.article_id} is not a valid article id`});
+      else return next({status: 500, message: 'server error'});
+      });
 };
