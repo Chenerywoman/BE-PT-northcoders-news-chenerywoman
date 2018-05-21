fe219f6c4325a8a33332734cdb8394a8918029cc
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-21 11:48:32 +0100

    updating updateCommentVote with a ternary operator for change value plus additional argument to return updated comment

--- queries/comments.queries.js
+++ queries/comments.queries.js
@@ -6,4 +6,11 @@ exports.findCommentsForArticle = (belongs_to) => Comment.find({belongs_to});
 
 exports.createComment = (body, belongs_to, created_by) => Comment.create({body, belongs_to, created_by});
 
-exports.updateCommentVote  = (_id, votes) => Comment.findByIdAndUpdate(_id, {votes});
+exports.updateCommentVote  = (_id, vote) => {
+    // change variable takes a ternary operator if 'vote' (req.query.vote passed in as an argument in the changeCommentVotes in the controller)
+    // up 1, down -1 anything else - 0, i.e. doesn't change
+    const change = vote === 'up' ? 1 : vote === 'down' ? -1 : 0;
+    // pass the variable change to the increment function 
+    // {new:true} returns the updated version
+    return Comment.findByIdAndUpdate(_id, {$inc: {votes: change}}, {new: true});
+};
