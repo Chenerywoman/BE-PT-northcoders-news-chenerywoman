9d58124e3a8eca899745b1a1753906d40c59c775
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-21 20:07:07 +0100

    adding select & populate to findAllArticles to remove __v and populate _id & title fields of belongs to & _id & username of created_by

--- queries/articles.queries.js
+++ queries/articles.queries.js
@@ -1,6 +1,6 @@
 const {Article} = require('../models');
 
-exports.findAllArticles = () => Article.find();
+exports.findAllArticles = () => Article.find().select('-__v').populate('belongs_to', ['_id', 'title']).populate('created_by', ['_id', 'username']);
 
 exports.findArticleById = (id) => Article.findById(id);
 
