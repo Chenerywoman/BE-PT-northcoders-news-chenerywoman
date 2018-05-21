34cc9dd91dafdaff1c7dd82296f7bfa06433b46c
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-21 11:47:33 +0100

    updating changeCommentVotes function to return an unchanged comment rather than an error when invalid put request

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -3,14 +3,12 @@ const {findCommentById, updateCommentVote} = require('../queries/comments.querie
 exports.changeCommentVotes = (req, res, next) => {
     return findCommentById(req.params.comment_id)
     .then(comment => {
-      const {vote} = req.query;
-      if (vote !== 'up' && vote !== 'down') throw {status: 400, message: 'query string parameter "vote" must be "up" or "down"'}
-      else if (vote === 'up') comment.votes = comment.votes + 1;
-      else if (vote === 'down') comment.votes = comment.votes - 1;
-      return updateCommentVote(comment._id, comment.votes);
+      return updateCommentVote(comment._id, req.query.vote);
       })
-    .then(comment => findCommentById(comment._id))
-    .then(comment => res.status(200).send({updated_comment: comment}))
+    .then(comment => {
+      // n.b. if invalid key or value put into query string parameter, just returns the original comment unchanged
+      res.status(200).send({updated_comment: comment});
+    })
     .catch((err) => { 
       if (err.name === 'CastError' && err.model.modelName === 'comments') return next({ status: 400,  message: `${req.params.comment_id} is not a valid comment id`});
       else if (err.status === 400) return next(err);
