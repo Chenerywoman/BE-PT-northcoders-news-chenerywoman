1f89cfe387682d17de55dbc5906cb84471c3d6d1
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-21 14:36:31 +0100

    adding a route for getCommentById

--- routers/comments.router.js
+++ routers/comments.router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { changeCommentVotes, deleteComment} = require('../controllers/comments.controller');
+const { changeCommentVotes, deleteComment, getCommentById} = require('../controllers/comments.controller');
+
+router.get('/:comment_id', getCommentById);
 
 router.put('/:comment_id', changeCommentVotes);
 
