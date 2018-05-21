358eee567401f914b97bed3967417fa34afea42a
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-21 01:03:08 +0100

    updateCommentVotes function passing tests

--- queries/comments.queries.js
+++ queries/comments.queries.js
@@ -1,5 +1,9 @@
 const {Comment} = require('../models');
 
+exports.findCommentById = (_id) => Comment.findById(_id);
+
 exports.findCommentsForArticle = (belongs_to) => Comment.find({belongs_to});
 
 exports.createComment = (body, belongs_to, created_by) => Comment.create({body, belongs_to, created_by});
+
+exports.updateCommentVote  = (_id, votes) => Comment.findByIdAndUpdate(_id, {votes});
