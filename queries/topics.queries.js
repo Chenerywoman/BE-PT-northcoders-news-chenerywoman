9a567eb7aefcd7cddfe12cf2e682234b0e7ba5f2
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-23 13:54:12 +0100

    adding populate to function to get articles for a topic

--- queries/topics.queries.js
+++ queries/topics.queries.js
@@ -4,4 +4,4 @@ exports.findAllTopics = () => Topic.find();
 
 exports.findTopicById = (id) => Topic.findById(id);
 
-exports.findArticlesByTopicId = (belongs_to) => Article.find({belongs_to})
+exports.findArticlesByTopicId = (belongs_to) => Article.find({belongs_to}).select('-__v').populate('belongs_to', ['_id', 'title']).populate('created_by', ['_id', 'username']);
