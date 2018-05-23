c63facd2c6de60001133dfc87d8f521d59b00b66
Chenerywoman <rachelmchenery@hotmail.com>
2018-05-23 19:12:33 +0100

    adding code to enable switching between PORT 3000  & Heroku port

--- listen.js
+++ listen.js
@@ -1,6 +1,12 @@
 const app = require('./app');
+// sets a variable PORT with a value of 300 - for use from the local host
+let PORT = 3000 
 
-app.listen(3000, function (err) {if (err) 
+// when deploying to heroku, the node env will be production.  In that case, change the value of PORT to process.env.PORT, which heroku will recognise.
+if (process.env.NODE_ENV === 'production') {PORT = process.env.PORT;}
+
+// app will now listen either on port 3000 or the heroku port
+app.listen(PORT, function (err) {if (err) 
   return console.log(err);
   console.log('App listening on port 3000');
 });
