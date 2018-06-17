7b7611a571a2cb29c2ef4e4575aed10f6fb21a09
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-17 21:00:46 +0100

    removing comments

--- queries/articles.queries.js
+++ queries/articles.queries.js
@@ -8,10 +8,6 @@ exports.findArticleById = (id) => Article.findById(id).select('-__v').populate('
 exports.createArticle = (title, body, belongs_to, created_by) => Article.create({title, body, belongs_to, created_by});
 
 exports.updateArticleVote  = (_id, vote) => {
-    // change variable takes a ternary operator if 'vote' (req.query.vote passed in as an argument in the changeCommentVotes in the controller)
-    // up 1, down -1 anything else - 0, i.e. doesn't change
     const change = vote === 'up' ? 1 : vote === 'down' ? -1 : 0;
-    // pass the variable change to the increment function 
-    // {new:true} returns the updated version
     return Article.findByIdAndUpdate(_id, {$inc: {votes: change}}, {new: true}).lean();
 };
