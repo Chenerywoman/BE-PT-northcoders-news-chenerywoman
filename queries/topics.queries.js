b2ca1b97eb1a778f18f472d218565bc1ad85f44d
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-17 21:05:52 +0100

    adding lean to queries

--- queries/topics.queries.js
+++ queries/topics.queries.js
@@ -1,7 +1,7 @@
 const {Topic, Article} = require('../models/');
 
-exports.findAllTopics = () => Topic.find();
+exports.findAllTopics = () => Topic.find().lean();
 
-exports.findTopicById = (id) => Topic.findById(id);
+exports.findTopicById = (id) => Topic.findById(id).lean();
 
-exports.findArticlesByTopicId = (belongs_to) => Article.find({belongs_to}).select('-__v').populate('belongs_to', ['_id', 'title']).populate('created_by', ['_id', 'username']);
+exports.findArticlesByTopicId = (belongs_to) => Article.find({belongs_to}).select('-__v').populate('belongs_to', ['_id', 'title']).populate('created_by', ['_id', 'username']).lean();
