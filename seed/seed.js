ad605420270510c70968ac9f4495df7839e9c2f4
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-17 16:59:00 +0100

    reformatting

--- seed/seed.js
+++ seed/seed.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
-const {Topic, User, Article, Comment} = require('../models');
+const { Topic, User, Article, Comment } = require('../models');
 
-exports.seed = (topicsData, usersData, articlesData, commentsData) =>  {
+exports.seed = (topicsData, usersData, articlesData, commentsData) => {
     return mongoose.connection.dropDatabase()
 
-    .then(() => Promise.all([Topic.insertMany(topicsData), User.insertMany(usersData)]))
-    .then(([topicsDocs, usersDocs]) => {
+        .then(() => Promise.all([Topic.insertMany(topicsData), User.insertMany(usersData)]))
+        .then(([topicsDocs, usersDocs]) => {
             const articles = articlesData.map(article => {
                 const created_by = usersDocs.find(user => user.username === article.created_by)._id;
                 const belongs_to = topicsDocs.find(topic => topic.slug === article.topic)._id;
                 const votes = Math.floor(Math.random() * 30)
-                return { ...article, created_by, belongs_to, votes}
+                return { ...article, created_by, belongs_to, votes }
             });
             return Promise.all([topicsDocs, usersDocs, Article.insertMany(articles)]);
-       })
-       .then(([topicsDocs, usersDocs, articlesDocs]) => {
+        })
+        .then(([topicsDocs, usersDocs, articlesDocs]) => {
             const comments = commentsData.map(comment => {
                 const belongs_to = articlesDocs.find(article => article.title === comment.belongs_to)._id;
                 const created_by = usersDocs.find(user => user.username === comment.created_by)._id;
-                return { ...comment, created_by, belongs_to};
+                return { ...comment, created_by, belongs_to };
             });
             return Promise.all([topicsDocs, usersDocs, articlesDocs, Comment.insertMany(comments)])
-       });
-    };
+        });
+};
