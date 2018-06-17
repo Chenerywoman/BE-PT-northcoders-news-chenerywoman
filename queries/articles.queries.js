a192d28f2cd558248666a4c9a83abbbc789ae09a
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-17 20:55:33 +0100

    adding lean to findArticleById & updateArticleVote

--- queries/articles.queries.js
+++ queries/articles.queries.js
@@ -2,8 +2,9 @@ const {Article} = require('../models');
 
 exports.findAllArticles = () => Article.find().lean().select('-__v').populate('belongs_to', ['_id', 'title']).populate('created_by', ['_id', 'username']).lean();
 
-exports.findArticleById = (id) => Article.findById(id).select('-__v').populate('belongs_to', ['_id', 'title']).populate('created_by', ['_id', 'username']);
+exports.findArticleById = (id) => Article.findById(id).select('-__v').populate('belongs_to', ['_id', 'title']).populate('created_by', ['_id', 'username']).lean();
 
+// n.b. with .lean() this returns a server error
 exports.createArticle = (title, body, belongs_to, created_by) => Article.create({title, body, belongs_to, created_by});
 
 exports.updateArticleVote  = (_id, vote) => {
@@ -12,5 +13,5 @@ exports.updateArticleVote  = (_id, vote) => {
     const change = vote === 'up' ? 1 : vote === 'down' ? -1 : 0;
     // pass the variable change to the increment function 
     // {new:true} returns the updated version
-    return Article.findByIdAndUpdate(_id, {$inc: {votes: change}}, {new: true});
+    return Article.findByIdAndUpdate(_id, {$inc: {votes: change}}, {new: true}).lean();
 };
