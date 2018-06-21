8bc442882f0d33f3dbefab32b8b7391a6d638872
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-21 12:42:02 +0100

    standardising 500 errors & some refactoring functions onto same line

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -3,7 +3,7 @@ const {findCommentById, updateCommentVote, deleteComment, findComments} = requir
 exports.getComments = (req, res, next) => {
   return findComments()
     .then(comments => res.status(200).send({comments}))
-    .catch(() =>  next({status: 500, message: 'server error - unable get comments'}));
+    .catch(() =>  next({status: 500, controller: 'comments'}));
 };
 
 exports.getCommentById = (req, res, next) => {
@@ -11,22 +11,17 @@ exports.getCommentById = (req, res, next) => {
     .then(comment  => res.status(200).send({comment}))
     .catch(err => {
       if (err.name === 'CastError' && err.model.modelName === 'comments') return next({ status: 400,  message: `${req.params.comment_id} is not a valid comment id`});
-      else return next({status: 500, message: 'server error - unable to delete'});
+      else return next({status: 500, controller: 'comments'});
     });
 };
 
 exports.changeCommentVotes = (req, res, next) => {
     return findCommentById(req.params.comment_id)
-    .then(comment => {
-      return updateCommentVote(comment._id, req.query.vote);
-      })
-    .then(comment => {
-      // n.b. if invalid key or value put into query string parameter, just returns the original comment unchanged
-      res.status(200).send({updated_comment: comment});
-    })
+    .then(comment => updateCommentVote(comment._id, req.query.vote))
+    .then(comment => res.status(200).send({updated_comment: comment}))
     .catch(err => { 
       if (err.name === 'CastError' && err.model.modelName === 'comments') return next({ status: 400,  message: `${req.params.comment_id} is not a valid comment id`});
-      else return next({status: 500, message: 'server error'});
+      else return next({status: 500, controller: 'comments'});
       });
 };
 
@@ -40,6 +35,6 @@ exports.deleteComment = (req, res, next) => {
     .catch(err => {
       if (err.status === 404) return next(err);
       else if (err.name === 'CastError' && err.model.modelName === 'comments') return next({ status: 400,  message: `${req.params.comment_id} is not a valid comment id`});
-      else return next({status: 500, message: 'server error - unable to delete'});
+      else return next({status: 500, controller: 'comments'});
     });
 };
