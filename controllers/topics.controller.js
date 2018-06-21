0f521b3e400b80a2ef6f9d6b255658778661bea0
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-21 12:56:10 +0100

    standardising 500 errors & refactoring some functions onto one line

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -5,7 +5,7 @@ const {createArticle} = require('../queries/articles.queries');
 exports.getAllTopics = (req, res, next) => {
   return findAllTopics()
   .then(topics => res.status(200).send({topics}))
-  .catch(() => next({status: 500, message: 'server error: unable to find topics'}))
+  .catch(() => next({status: 500, controller: 'topics'}));
 };
   
 exports.getArticlesByTopicId = (req, res, next) => {
@@ -18,22 +18,19 @@ exports.getArticlesByTopicId = (req, res, next) => {
     .catch((err) => { 
       if (err.name === 'CastError') return next({status: 400, message: 'please input a valid topic id'});
       else if (err.status === 404) return next(err);
-      else return next({status: 500, message: 'server error'})
+      else return next({status: 500, controller: 'topics'});
       });
 }; 
 
 exports.postNewArticleToTopic = (req, res, next) => {
   return findTopicById(req.params.topic_id)
-  .then(topic => {
-    return Promise.all([topic, findUserById(req.body.created_by)]);
-  })
-  .then(([topic, user]) => {
-     return createArticle(req.body.title, req.body.body, topic._id, user._id);
-  })
+  .then(topic =>  Promise.all([topic, findUserById(req.body.created_by)]))
+    // change to a specific default user rather than using one - object posting wouldn't have the created_by
+  .then(([topic, user]) => createArticle(req.body.title, req.body.body, topic._id, user._id))
   .then(article => res.status(201).send({new_article: article}))
   .catch((err) => { 
     if (err.name === 'CastError' && err.model.modelName === 'topics') return next({ status: 400,  message: `${req.params.topic_id} is not a valid topic id`});
     else if (err.name === 'CastError' && err.model.modelName === 'users') return next({ status: 400,  message: `${req.body.created_by} is not a valid user id`});
-    else return next({status: 500, message: 'server error'});
+    else return next({status: 500, controller: 'topics'});
     });
   };
