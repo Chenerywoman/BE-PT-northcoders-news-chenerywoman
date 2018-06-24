30737b1c1c8d0b0546d827eafd4ad63b47fc1076
Chenerywoman <rachelmchenery@hotmail.com>
2018-06-24 18:41:57 +0100

    removing hardcoded strings in tests & changing toString() to template literals for ids

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -23,13 +23,11 @@ describe('API endpoints', () => {
                 .expect(200)
                 .then(res => {
                     const { topics } = res.body;
-                    expect(topics).to.be.an('array');
                     expect(topics.length).to.equal(3);
                     expect(topics[0].title).to.equal(topicDocs[0].title);
                     expect(topics[0].slug).to.equal(topicDocs[0].slug);
-                    expect(topics[0]._id).to.equal(topicDocs[0]._id.toString());
+                    expect(topics[0]._id).to.equal(`${topicDocs[0]._id}`);
                     expect(topics[0]).to.have.keys('_id', 'title', 'slug', '__v');
-                    expect(topics[0].title).to.equal('Mitch');
                 });
         });
         it('GETs all articles for a topic from api/topics/:topic_id/articles', () => {
@@ -38,14 +36,12 @@ describe('API endpoints', () => {
                 .expect(200)
                 .then(res => {
                     const { articles } = res.body;
-                    expect(articles).to.be.an('array');
                     expect(articles.length).to.equal(2);
                     expect(articles[0].votes).to.equal(articleDocs[0].votes);
                     expect(articles[0].title).to.equal(articleDocs[0].title);
                     expect(articles[0].body).to.equal(articleDocs[0].body);
-                    expect(articles[0].belongs_to._id).to.equal(articleDocs[0].belongs_to.toString());
+                    expect(articles[0].belongs_to._id).to.equal(`${articleDocs[0].belongs_to}`);
                     expect(articles[0]).to.have.keys('_id', 'title', 'created_by', 'body', 'belongs_to', 'votes');
-                    expect(articles[0].title).to.equal('Living in the shadow of a great man');
                 });
         });
         it('returns an appropriate error message if an invalid topic id inputted as a parameter', () => {
@@ -71,10 +67,8 @@ describe('API endpoints', () => {
                     const { new_article } = res.body;
                     expect(new_article.title).to.equal(newArticle.title);
                     expect(new_article.body).to.equal(newArticle.body);
-                    expect(new_article.created_by).to.equal(newArticle.created_by.toString());
+                    expect(new_article.created_by).to.equal(`${newArticle.created_by}`);
                     expect(new_article).to.have.keys('_id', 'title', 'created_by', 'body', 'belongs_to', 'votes', '__v');
-                    expect(new_article.title).to.equal('Why I love Penguins');
-                    expect(new_article.body).to.equal('blah, blah, Penguins, blah, more penguins, love them');
                 });
         });
         it('responds with an appropriate error to a POST request if an invalid topic id is passed as a parameter', () => {
@@ -102,14 +96,12 @@ describe('API endpoints', () => {
                 .expect(200)
                 .then(res => {
                     const { articles } = res.body;
-                    expect(articles).to.be.an('array');
                     expect(articles.length).to.equal(5);
-                    expect(articles[3]._id).to.equal(articleDocs[3]._id.toString());
+                    expect(articles[3]._id).to.equal(`${articleDocs[3]._id}`);
                     expect(articles[3].title).to.equal(articleDocs[3].title);
                     expect(articles[3].body).to.equal(articleDocs[3].body);
-                    expect(articles[3].belongs_to._id).to.equal(articleDocs[3].belongs_to.toString());
+                    expect(articles[3].belongs_to._id).to.equal(`${articleDocs[3].belongs_to}`);
                     expect(articles[3]).to.have.keys('_id', 'title', 'created_by', 'body', 'belongs_to', 'votes', 'comments');
-                    expect(articles[3].title).to.equal('UNCOVERED: catspiracy to bring down democracy');
                     expect(articles[3].created_by).to.have.keys('_id', 'username');
                     expect(articles[3].belongs_to).to.have.keys('_id', 'title');
                     expect(articles[3].comments).to.be.a('number');
@@ -121,14 +113,13 @@ describe('API endpoints', () => {
                 .expect(200)
                 .then(res => {
                     const { article } = res.body;
-                    expect(article._id).to.equal(articleDocs[1]._id.toString());
+                    expect(article._id).to.equal(`${articleDocs[1]._id}`);
                     expect(article.title).to.equal(articleDocs[1].title);
                     expect(article.body).to.equal(articleDocs[1].body);
-                    expect(article.created_by._id).to.equal(articleDocs[1].created_by.toString());
-                    expect(article.belongs_to._id).to.equal(articleDocs[1].belongs_to.toString());
+                    expect(article.created_by._id).to.equal(`${articleDocs[1].created_by}`);
+                    expect(article.belongs_to._id).to.equal(`${articleDocs[1].belongs_to}`);
                     expect(article.votes).to.equal(articleDocs[1].votes);
                     expect(article).to.have.keys('_id', 'title', 'created_by', 'body', 'belongs_to', 'votes', 'comments');
-                    expect(article.title).to.equal('7 inspirational thought leaders from Manchester UK');
                     expect(article.created_by).to.have.keys('_id', 'username');
                     expect(article.belongs_to).to.have.keys('_id', 'title');
                 });
@@ -145,15 +136,13 @@ describe('API endpoints', () => {
                 .expect(200)
                 .then(res => {
                     const { comments } = res.body;
-                    expect(comments).to.be.an('array');
                     expect(comments.length).to.equal(2);
-                    expect(comments[0]._id).to.equal(commentDocs[2]._id.toString());
-                    expect(comments[0].created_by._id).to.equal(commentDocs[2].created_by.toString());
+                    expect(comments[0]._id).to.equal(`${commentDocs[2]._id}`);
+                    expect(comments[0].created_by._id).to.equal(`${commentDocs[2].created_by}`);
                     expect(comments[0].body).to.equal(commentDocs[2].body);
-                    expect(comments[0].belongs_to._id).to.equal(commentDocs[2].belongs_to.toString());
+                    expect(comments[0].belongs_to._id).to.equal(`${commentDocs[2].belongs_to}`);
                     expect(comments[0].votes).to.equal(commentDocs[2].votes);
                     expect(comments[0]).to.have.keys('_id', 'created_by', 'body', 'belongs_to', 'votes', 'created_at');
-                    expect(comments[0].body).to.equal('The owls are not what they seem.');
                     expect(comments[0].created_by).to.have.keys('_id', 'username');
                     expect(comments[0].belongs_to).to.have.keys('_id', 'title');
                 });
@@ -174,9 +163,8 @@ describe('API endpoints', () => {
                 .then(res => {
                     const { new_comment } = res.body;
                     expect(new_comment.body).to.equal(newComment.body);
-                    expect(new_comment.created_by).to.equal(newComment.created_by.toString());
+                    expect(new_comment.created_by).to.equal(`${newComment.created_by}`);
                     expect(new_comment).to.have.keys('_id', 'created_by', 'body', 'belongs_to', 'votes', '__v', 'created_at');
-                    expect(new_comment.body).to.equal('personally I\'d rather have a nice cup of tea');
                 });
         });
         it('responds with an appropriate error to a POST request if an invalid article id is passed as a parameter', () => {
@@ -260,14 +248,12 @@ describe('API endpoints', () => {
                 .expect(200)
                 .then(res => {
                     const { users } = res.body;
-                    expect(users).to.be.an('array');
                     expect(users.length).to.equal(2);
                     expect(users[0].username).to.equal(userDocs[0].username);
                     expect(users[0].avatar_url).to.equal(userDocs[0].avatar_url);
                     expect(users[0].name).to.equal(userDocs[0].name);
-                    expect(users[0]._id).to.equal(userDocs[0]._id.toString());
+                    expect(users[0]._id).to.equal(`${userDocs[0]._id}`);
                     expect(users[0]).to.have.keys('_id', '__v', 'avatar_url', 'name', 'username');
-                    expect(users[0].username).to.equal('butter_bridge');
                 });
         });
         it('GETs a user by username from api/users/username/:username', () => {
@@ -277,12 +263,11 @@ describe('API endpoints', () => {
                 .then(res => {
                     const { user } = res.body;
                     expect(user.username).to.equal(userDocs[1].username);
-                    expect(user._id).to.equal(userDocs[1]._id.toString());
+                    expect(user._id).to.equal(`${userDocs[1]._id}`);
                     expect(user.avatar_url).to.equal(userDocs[1].avatar_url);
                     expect(user.name).to.equal(userDocs[1].name);
                     expect(user.username).to.equal(userDocs[1].username);
                     expect(user).to.have.keys('_id', '__v', 'avatar_url', 'name', 'username');
-                    expect(user.username).to.equal('dedekind561');
                 });
         });
         it('returns an appropriate error message if non-existent username inputted', () => {
@@ -297,12 +282,11 @@ describe('API endpoints', () => {
                 .expect(200)
                 .then(res => {
                     const { user } = res.body;
-                    expect(user._id).to.equal(userDocs[1]._id.toString());
+                    expect(user._id).to.equal(`${userDocs[1]._id}`);
                     expect(user.avatar_url).to.equal(userDocs[1].avatar_url);
                     expect(user.name).to.equal(userDocs[1].name);
                     expect(user.username).to.equal(userDocs[1].username);
                     expect(user).to.have.keys('_id', '__v', 'avatar_url', 'name', 'username');
-                    expect(user.username).to.equal('dedekind561');
                 });
         });
         it('returns an appropriate error message if non-existent id inputted', () => {
@@ -321,12 +305,11 @@ describe('API endpoints', () => {
                 .expect(200)
                 .then(res => {
                     const { comments } = res.body;
-                    expect(comments).to.be.an('array');
                     expect(comments.length).to.equal(8);
-                    expect(comments[3]._id).to.equal(commentDocs[3]._id.toString());
-                    expect(comments[3].created_by._id).to.equal(commentDocs[3].created_by.toString());
+                    expect(comments[3]._id).to.equal(`${commentDocs[3]._id}`);
+                    expect(comments[3].created_by._id).to.equal(`${commentDocs[3].created_by}`);
                     expect(comments[3].body).to.equal(commentDocs[3].body);
-                    expect(comments[3].belongs_to._id).to.equal(commentDocs[3].belongs_to.toString());
+                    expect(comments[3].belongs_to._id).to.equal(`${commentDocs[3].belongs_to}`);
                     expect(comments[3].votes).to.equal(commentDocs[3].votes);
                     expect(comments[3].belongs_to).to.have.keys('_id', 'title');
                     expect(comments[3]).to.have.keys('_id', 'created_by', 'body', 'belongs_to', 'votes', 'created_at');
@@ -342,8 +325,8 @@ describe('API endpoints', () => {
                 .then(res => {
                     const { comment } = res.body;
                     expect(comment).to.have.keys('_id', 'created_by', 'body', 'belongs_to', 'votes', 'created_at');
-                    expect(comment._id).to.equal(commentDocs[4]._id.toString());
-                    expect(comment.created_by._id).to.equal(commentDocs[4].created_by.toString());
+                    expect(comment._id).to.equal(`${commentDocs[4]._id}`);
+                    expect(comment.created_by._id).to.equal(`${commentDocs[4].created_by}`);
                     expect(comment.body).to.equal(commentDocs[4].body);
                     expect(comment.belongs_to._id).to.equal(`${commentDocs[4].belongs_to}`);
                     expect(comment.votes).to.equal(commentDocs[4].votes);
@@ -408,7 +391,6 @@ describe('API endpoints', () => {
             return request
                 .put(`/api/comments/${commentDocs[2]._id}`)
                 .set('Accept', 'application/json')
-                // query string parameter
                 .query({ july: 'up' })
                 .expect(200)
                 .then(res => {
